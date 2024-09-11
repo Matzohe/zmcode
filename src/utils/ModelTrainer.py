04f361ea716ec819f3d09c8bd98005f79ba4cc62
Matzoh <mzimo@foxmail.com>
2024-09-11 16:50:46 +0800

    添加训练检查点以及从检查点恢复训练，同时添加了tensorboard

--- src/utils/ModelTrainer.py
+++ src/utils/ModelTrainer.py
@@ -3,21 +3,67 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from typing import Union, List, Dict
 
+import os
+from tqdm import tqdm
+import time
+
+from .CheckPointUtils import save_checkpoint
 
 # TODO: Add UnsupervisedModelTrainer
-def SupervisedModelTrainer(
+def BasicSupervisedModelTrainer(
     config,
     model: nn.Module,
-    train_dataset: torch.utils.data.Dataset,
-    val_dataset: torch.utils.data.Dataset,
+    train_dataloader: torch.nn.utils.Dataset,
     optimizer: torch.optim.Optimizer,
     loss_fn: torch.nn.Module,
-    summary_writer: torch.utils.tensorboard.SummaryWriter
+    summary_writer: Union[torch.utils.tensorboard.SummaryWriter] = None,
+    from_checkpoint: bool = False,
+    checkpoint_path: str = None
 ):
     # This function has a limitation, the model's output should be the same as the label
     epochs = int(config.MODEL['epoch'])
-
-    for epoch in range(epochs):
-        pass
     
+    if from_checkpoint:
+        try:
+            checkpoint = torch.load(checkpoint_path)
+        except:
+            raise RuntimeError("Something wrong with checkpoint loading, please check your checkpoint path")
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        before_batch_idx = checkpoint['batch_idx']
+        before_epoch_idx = checkpoint['epoch_idx']
+        print("Checkpoint loaded at {}".format(checkpoint_path))
+    
+    # TODO: Shuffle the training datasets at the start of the epoch, need to change the training logics
+    for epoch in range(epochs):
+        if from_checkpoint & before_epoch_idx > epoch:
+            continue
+        start_time = time.perf_counter()
+        for batch_num, (_x, _y) in tqdm(train_dataloader, desc=f"Epoch {epoch}", total=len(train_dataloader)):
+            if from_checkpoint & before_batch_idx > batch_num:
+                continue
+            if summary_writer is not None:
+                if not os.path.exists(config["TRAINING"]["log_dir"]):
+                    try:
+                        os.mkdir(config["TRAINING"]["log_dir"])
+                    except:
+                        raise ValueError("Can't create log dir, you need to create the root path before create the log file")
+                _writer = SummaryWriter(config["TRAINING"]["log_dir"])
+            output_y = model(_x)
+            loss = loss_fn(output_y, _y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # write training loss to tensorboard
+            if summary_writer is not None:
+                _writer.add_scalar("loss", loss, epoch)
+            end_time = time.perf_counter()
+
+            # save training check point
+            if end_time - start_time > int(config["TRAINING"]["checkpoint_save_time"]):
+                save_checkpoint(config, model, optimizer, batch_num, epoch)
+                start_time = time.perf_counter()
