91d432eec1217f2847f5caa080c7c4dee4b63e64
Matzoh <mzimo@foxmail.com>
2024-09-20 16:04:47 +0800

    添加Imagenet数据导入（未完成），增加Imagenet数据处理（和coco相同）

--- src/utils/ImagePreporcessUitls.py
+++ src/utils/ImagePreporcessUitls.py
@@ -23,3 +23,18 @@ def ClipPreProcess(image_path: str) -> torch.Tensor:
     ])
 
     return preprocess(image).unsqueeze(0)
+
+
+def ImageNetPreProcess(image_path: str) -> torch.Tensor:
+    # ImageNet image preprocess function
+    # The hyperparameters in the Normalize is the average and standard deviation of ImageNet
+    image = Image.open(image_path)
+    if image.mode == "L":
+        image = image.convert("RGB")
+    preprocess = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    ])
+
+    return preprocess(image).unsqueeze(0)
