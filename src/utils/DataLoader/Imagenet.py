73ea57260536db97986aff90b49df0d741736ff4
Matzoh <mzimo@foxmail.com>
2024-09-28 13:34:36 +0800

    添加ImageNet的标签处理

--- src/utils/DataLoader/Imagenet.py
+++ src/utils/DataLoader/Imagenet.py
@@ -1,5 +1,31 @@
 import torch
 import torch.nn as nn
+from torch.utils.data import Dataset
 import torchvision.transforms as transforms
+from collections import OrderedDict
+import os
 
 from ..ImagePreporcessUitls import ImageNetPreProcess
+
+
+def get_classification(config):
+    # caffe file is downloaded from url: http://dl.caffe.berkeleyvision.org/caffe_ilsvrc12.tar.gz
+    caffe_root = config.DATASET["imagenet_caffe"]
+    labels = OrderedDict()
+    with open(os.path.join(caffe_root, "synsets.txt"), "r") as f:
+        for i, line in enumerate(f):
+            label = line.strip()
+            labels[label] = i
+
+    return labels
+
+
+class ImageNetDataset(Dataset):
+    def __init__(self, config):
+        pass
+
+    def __getitem__():
+        pass
+
+    def __len__():
+        pass
