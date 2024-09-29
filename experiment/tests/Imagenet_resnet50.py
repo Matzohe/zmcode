f274d66b2e81b8a52112c7cf4edd566a5d2349ee
Matzoh <mzimo@foxmail.com>
2024-09-29 20:46:47 +0800

    resnet50训练函数完成

---
+++ experiment/tests/Imagenet_resnet50.py
@@ -0,0 +1,17 @@
+from src.CV.ResNet50 import ResNet
+from src.utils.OptimizerUtils import configure_optimizers
+from src.utils.ModelTrainer import BasicSupervisedModelTrainer
+from src.utils.utils import set_seed, INIconfig
+from src.utils.DataLoader.ImageNet import get_imagenet_training_dataloader, get_imagenet_validation_dataloader
+import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
+
+def Imagenet_train_resnet50_test():
+    config = INIconfig('config.cfg')
+    model = ResNet(config).to(device=config.TRAINING['device'])
+    train_dataloader = get_imagenet_training_dataloader(config)
+    val_dataloader = get_imagenet_validation_dataloader(config)
+    optimizer = configure_optimizers(model, config)
+    loss_fn = nn.CrossEntropyLoss()
+    summary_writer = SummaryWriter()
+    BasicSupervisedModelTrainer(config=config, model=model, train_dataloader=train_dataloader, optimizer=optimizer, loss_fn=loss_fn, summary_writer=summary_writer, val_dataloader=val_dataloader)
