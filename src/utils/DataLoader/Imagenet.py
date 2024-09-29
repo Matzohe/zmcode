096afd4818f25b6d1f5c5a2de954699206a45092
Matzoh <mzimo@foxmail.com>
2024-09-29 20:10:22 +0800

    修改ImageNet Dataloader中的bug

--- src/utils/DataLoader/Imagenet.py
+++ src/utils/DataLoader/Imagenet.py
@@ -25,22 +25,25 @@ def get_train_list(config):
     # caffe file is downloaded from url: http://dl.caffe.berkeleyvision.org/caffe_ilsvrc12.tar.gz
     caffe_root = config.DATASET["imagenet_caffe"]
     root_list = []
+    label_list = []
     with open(os.path.join(caffe_root, "train.txt"), "r") as f:
         for line in f:
-            root_list.append(line.strip())
-
-    return root_list
+            root_list.append(line.split(" ")[0])
+            label_list.append(int(line.split(" ")[1]))
+    return root_list, label_list
 
 
 def get_val_list(config):
     # caffe file is downloaded from url: http://dl.caffe.berkeleyvision.org/caffe_ilsvrc12.tar.gz
     caffe_root = config.DATASET["imagenet_caffe"]
     val_list = []
+    label_list = []
     with open(os.path.join(caffe_root, "val.txt"), "r") as f:
         for line in f:
-            val_list.append(line.strip())
+            val_list.append(line.split(" ")[0])
+            label_list.append(int(line.split(" ")[1]))
 
-    return val_list
+    return val_list, label_list
 
 class ImageNetTrainDataset(Dataset):
     def __init__(self, config):
@@ -49,11 +52,13 @@ class ImageNetTrainDataset(Dataset):
         self.config = config
         self.imagenet_root = config.DATASET["imagenet_root"]
         
-        self.root_list = [os.path.join(self.imagenet_root, root) for root in get_train_list(config)]
+        self.root_list, self.label_list = get_train_list(config)
+        self.root_list = [os.path.join(self.imagenet_root, x) for x in self.root_list]
+
         random.shuffle(self.root_list)
 
     def __getitem__(self, index):
-        return ImageNetPreProcess(self.root_list[index])
+        return ImageNetPreProcess(self.root_list[index]), self.label_list[index]
 
     def __len__(self):
         return len(self.root_list)
@@ -66,11 +71,13 @@ class ImageNetValDataset(Dataset):
         self.config = config
         self.imagenet_root = config.DATASET["imagenet_root"]
         
-        self.root_list = [os.path.join(self.imagenet_root, root) for root in get_val_list(config)]
+        self.root_list, self.label_list = get_val_list(config)
+        self.root_list = [os.path.join(self.imagenet_root, x) for x in self.root_list]
+        
         random.shuffle(self.root_list)
 
     def __getitem__(self, index):
-        return ImageNetPreProcess(self.root_list[index])
+        return ImageNetPreProcess(self.root_list[index]), self.label_list[index]
 
     def __len__(self):
         return len(self.root_list)
