c9aa5e50663547b9a8cba778770c73c4265ab230
Matzoh <mzimo@foxmail.com>
2024-10-06 16:02:55 +0800

    限制为lz77

--- src/c++/CompressionTools/zipPression.cpp
+++ src/c++/CompressionTools/zipPression.cpp
@@ -7,7 +7,7 @@
 
 int main(){
 
-    std::ifstream file("/Users/a1/C_Program/Smart-Med-QA/cmake-build-debug/CMakeCache.txt");
+    std::ifstream file("/Users/a1/C_Program/个人代码+毛子鸣.zip");
     // 设置文件读写位置
     // 分多段进行压缩和解压缩
     file.seekg(0, std::ios::end);
@@ -31,28 +31,28 @@ int main(){
     std::string output_str = buffer_save.str();
     std::cout << output_str.size() << std::endl;
     std::string decoder_2 = rollingHash.DecodeLZ77WithInfo(output_str);
-    // std::ofstream decoded_file("output.txt", std::ios::app);
-    // decoded_file << decoder_2 << std::endl;
-    // decoded_file.close();
-    std::unordered_map<unsigned int, std::string> huffmanCode;
-    HuffmanTree huffmanTree(huffmanCode, nullptr);
-    std::unordered_map<unsigned int, long long> charcount = rollingHash.getCharcount(output_str);
-
-    std::cout << charcount.size() << std::endl;
-
-    huffmanTree.initializeHuffmanTree(charcount);
-
-    huffmanTree.encode(input_str);
-    
-    std::ifstream encoded_file("new_example.txt");
-    std::stringstream buffer_encode;
-    buffer_encode << encoded_file.rdbuf();
-    encoded_file.close();
-    std::string encoded_str = buffer_encode.str();
-    std::string decoder_1 = huffmanTree.decode(encoded_str);
-    std::ofstream decoded_file("output.txt", std::ios::app);
-    decoded_file << decoder_1 << std::endl;
+    std::ofstream decoded_file("个人代码+毛子鸣.zip", std::ios::app);
+    decoded_file << decoder_2 << std::endl;
     decoded_file.close();
+    // std::unordered_map<unsigned int, std::string> huffmanCode;
+    // HuffmanTree huffmanTree(huffmanCode, nullptr);
+    // std::unordered_map<unsigned int, long long> charcount = rollingHash.getCharcount(output_str);
+
+    // std::cout << charcount.size() << std::endl;
+
+    // huffmanTree.initializeHuffmanTree(charcount);
+
+    // huffmanTree.encode(input_str);
+    
+    // std::ifstream encoded_file("new_example.txt");
+    // std::stringstream buffer_encode;
+    // buffer_encode << encoded_file.rdbuf();
+    // encoded_file.close();
+    // std::string encoded_str = buffer_encode.str();
+    // std::string decoder_1 = huffmanTree.decode(encoded_str);
+    // std::ofstream decoded_file("output.txt", std::ios::app);
+    // decoded_file << decoder_1 << std::endl;
+    // decoded_file.close();
 
 
     // std::string decoder_2 = rollingHash.DecodeLZ77WithInfo(decoder_1);
