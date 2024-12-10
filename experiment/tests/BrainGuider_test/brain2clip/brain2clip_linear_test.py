9993b1ad99aaafafc92aa47ee5a522215b263dd9
Matzoh <mzimo@foxmail.com>
2024-12-10 19:14:09 +0800

    brain2clip test函数修改

--- experiment/tests/BrainGuider_test/brain2clip/brain2clip_linear_test.py
+++ experiment/tests/BrainGuider_test/brain2clip/brain2clip_linear_test.py
@@ -3,9 +3,9 @@ from src.Recognize.brain2clip.brain2clip import LinearBrain2CLIP
 from src.utils.utils import INIconfig
 
 def test_linear_brain2clip():
-    target_layer = ["visual.transformer.resblocks.{}".format(i) for i in range(12)]
-    mpl_layer = ["visual.transformers.resblocks.{}.mlp.c_proj".format(i) for i in range(12)]
+    target_layer = ["visual.transformer.resblocks.{}.mlp.c_proj".format(i) for i in range(12)]
     config = INIconfig("config/brain2clip_config.cfg")
     brain2clip = LinearBrain2CLIP(config)
-    for i in range(1, 9):
+    for i in range(1, 2):
         brain2clip.target_layer_linear_fitting(subj=i, voxel_activation_roi="ventral_visual_pathway_roi", target_layer=target_layer)
+
